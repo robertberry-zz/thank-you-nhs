4ed11cdc48b22ba33a7392991a2cb55633866943
Robert Berry <rjberry@gmail.com>
2015-03-15 12:02:49 +0000

    Set characters remaining red when 0

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,13 +1,40 @@
 import bean from 'bean';
 import bonzo from 'bonzo';
 import qwery from 'qwery';
+import forEach from 'lodash-node/modern/collection/forEach'
 
-export function bootstrap() {
-    let $button = bonzo(qwery('.js-open-form')[0]),
-        $form = bonzo(qwery('.js-form')[0]);
+const MAX_CHARS = 200;
+
+function $(selector) {
+    return bonzo(qwery(selector));
+}
+
+function addDisplayFormHandler() {
+    let $button = $('.js-open-form'),
+        $form = $('.js-form');
 
     bean.on($button[0], 'click', function () {
         $form.addClass('thank-you-message__form--displayed');
         $button.hide();
     });
+}
+
+function addRemainingCharactersHandler() {
+    let $messageBox = $('.js-message-box'),
+        $remainingChars = $('.js-remaining-chars');
+
+    forEach(['keydown', 'keyup'], function (event) {
+        bean.on($messageBox[0], event, function () {
+            const charsUsed = $messageBox.val().length,
+                  remaining = Math.max(0, MAX_CHARS - charsUsed);
+
+            $remainingChars.text(remaining);
+            $remainingChars.toggleClass('thank-you-message__characters-remaining--zero', remaining < 1);
+        });
+    });
+}
+
+export function bootstrap() {
+    addDisplayFormHandler();
+    addRemainingCharactersHandler();
 }
