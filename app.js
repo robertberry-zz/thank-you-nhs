a33c4c2633996980f173b3fa4a95e6ea6517da6d
Robert Berry <rjberry@gmail.com>
2015-03-15 11:14:07 +0000

    Set up jspm

---
+++ app.js
@@ -0,0 +1,9 @@
+(function () {
+
+    var openFormButton = document.getElementsByClassName("js-open-form")[0],
+        form = document.getElementsByClassName("js-form")[0];
+
+    openFormButton.onclick = function () {
+        console.log("Hi.");
+    }
+})();
